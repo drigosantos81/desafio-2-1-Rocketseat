426cc29f0ddca06a92d6cbbfc1bc1b83a77c0b67
Rodrigo Emanuel <drigosantos@gmail.com>
2020-02-17 09:48:59 -0300

    Configuração dos estilos

--- desafio-2.1.js
+++ desafio-2.1.js
@@ -1,10 +1,16 @@
-const modalPage = document.querySelector('.modal-1');
-const cards = document.querySelectorAll('.card-content');
+const modalPage = document.querySelector('.modal_1');
+const cards = document.querySelectorAll('.card__image-content');
 
 for (card of cards) {
-    document.addEventListener("click", function(){
-        const conteudo = card.getAttribute("conteudo");
+    card.addEventListener("click", function(){
+        const conteudo = card.getAttribute("id");
         modalPage.classList.add("active");
         modalPage.querySelector("iframe").src=`https://rocketseat.com.br/${conteudo}`;
     })
 }
+
+document.querySelector('.close-modal').addEventListener("click", function(event){
+    event.preventDefault();
+    modalPage.classList.remove("active");
+    modalPage.querySelector("iframe").src="";
+})
