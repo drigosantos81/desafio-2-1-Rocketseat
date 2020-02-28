627b618c6a41cb82e1645c08e7d00d880d6dcf01
Rodrigo Emanuel <drigosantos@gmail.com>
2020-02-28 13:50:46 -0300

    Correção na variavel do 'script.js'.

--- public/desafio-2.1.js
+++ public/desafio-2.1.js
@@ -1,7 +1,7 @@
 const modalPage = document.querySelector('.modal_1');
 const cards = document.querySelectorAll('.card__image-content');
 
-for (card of cards) {
+for (let card of cards) {
     card.addEventListener("click", function(){
         const conteudo = card.getAttribute("id");
         modalPage.classList.add("active");
